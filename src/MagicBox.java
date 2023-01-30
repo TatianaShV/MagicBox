f093efc120da5b89c78de900c6664764e702c081
TatianaShV <artemis1192@mail.ru>
2023-01-30 19:38:35 +0300

    init

---
+++ src/MagicBox.java
@@ -0,0 +1,37 @@
+import java.util.Random;
+
+public class MagicBox<T> {
+  //  private static final int SISE = 5;
+  //  protected int SIZE;
+    protected T[] items;
+    protected Random random = new Random();
+
+       public  MagicBox(int SIZE) {
+        this.items = (T[]) new Object[SIZE];
+            }
+
+    public boolean add(T item) {
+        for (int i = 0; i < items.length ; i++) {
+
+            if (items[i] == null) {
+                items[i] = item;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public T pick() {
+        int empty = 0;
+        for (T i : items) {
+            if (i == null) {
+                empty++;
+            }
+        }
+        if (empty > 0) {
+            throw new RuntimeException("Коробка не полна,есть свободные ячейки, осталось заполнить ещё " + empty);
+        }
+        int randomInt = random.nextInt(items.length);
+        return items[randomInt];
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        MagicBox <Integer> box1 = new MagicBox<>(5);
+        MagicBox <String> box2 = new MagicBox<>(3);
+        System.out.println(box1.add(956));
+        System.out.println(box1.add(5));
+        System.out.println(box1.add(66));
+
+        box2.add("hat");
+        box2.add("ring");
+        box2.add("ball");
+        System.out.println(box2.pick());
+        System.out.println(box1.pick());
+    }
+}
